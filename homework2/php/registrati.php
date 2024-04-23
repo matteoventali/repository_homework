4d0e037999949ec3f46533b0606a7fce6b8e67b9
Matteo <matteo@DESKTOP-A44G1DR.broadband>
2024-04-23 22:22:57 +0200

    Fine registrazione

--- homework2/php/registrati.php
+++ homework2/php/registrati.php
@@ -1,4 +1,68 @@
 <?php
+    // Variabile paragrafo risultato operazione
+    $ris = ''; $err = true;
+    
+    // Verifico se l'utente sia gia' loggato
+    // in caso viene ridirezionato sul menu dell'applicazione
+    session_start();
+    if ( isset($_SESSION["nome"]) )
+        header("Location: menu.php"); 
+    else if ( isset($_POST["nome"]) && isset($_POST["cognome"])
+                && isset($_POST["mail"]) && isset($_POST["password"]) )
+    {
+        // Elimino la sessione appena creata erroneamente
+        session_destroy();
+        setcookie(session_name(), '', time()-3600, '/');
+
+        // Se sono qui e' pervenuta una richiesta di registrazione
+        // Connessione al database
+        require_once 'connection.php';
+        
+        // Se la connessione e' andata a buon fine
+        if ( $connessione )
+        {
+            $ris = '<p style="color:red">Errore nell\'esecuzione della query, ricontrollare i dati</p>'; 
+
+            // Verifico che i dati siano corretti
+            // Regex: /^([a-z]+)(_|[.]|-){0,1}(([a-z]|\d)+)@([a-z])*[.]([a-z])*$/
+            $regex = '/^([a-z]+)(_|[.]|-){0,1}(([a-z]|\d)+)@([a-z])*[.]([a-z])*$/';
+            if ( preg_match($regex, $_POST["mail"]) )
+            {
+                // Prelevo i dati dal post
+                $nome = $handleDB->real_escape_string($_POST["nome"]);
+                $cognome = $handleDB->real_escape_string($_POST["cognome"]);
+                $mail = $handleDB->real_escape_string($_POST["mail"]);
+                $password = $handleDB->real_escape_string($_POST["password"]);
+                
+                // Compongo la query per l'esecuzione
+                $q = "insert into UTENTI(nome, cognome, mail, password) values ('$nome', '$cognome', '$mail', SHA2('$password', 256))";
+
+                // Esecuzione della query
+                try
+                {
+                    $handleDB->query($q);
+                    $ris = '<p style="color:green">Registrazione avvenuta con successo</p>';
+                    $err = false;
+                }
+                catch (Exception $e)
+                {
+                    $err = true;
+                    if ($handleDB->errno == 1062 )
+                        $ris = '<p style="color:red">e-mail presente nel database</p>';
+                }
+            }
+        }
+        else
+            $ris = '<p style="color:red">Errore di comunicazione con il database</p>';
+    }
+    else
+    {
+        // Elimino la sessione appena creata erroneamente
+        session_destroy();
+        setcookie(session_name(), '', time()-3600, '/');
+        $err = false;
+    }
+    
     echo '<?xml version = "1.0" encoding="ISO-8859-1"?>';
 ?>
 
@@ -30,21 +94,26 @@
 
         <!-- CONTENUTO CORPO PAGINA -->
         <div class="corpo">
-            <form method="get" action="">
+            <form method="post" action="<?php echo $_SERVER["PHP_SELF"] ?>">
                 <div class="contenutoForm">
                     <div class="riga">
-                        <p>Nome <input name="nome" type="text" /></p> 
-                        <p>Cognome <input name="cognome" type="text" /></p>
+                        <p>Nome <input name="nome" type="text" required="required" value="<?php if($err) echo $_POST["nome"];?>" /></p> 
+                        <p>Cognome <input name="cognome" type="text" required="required" value="<?php if($err) echo $_POST["cognome"];?>"/></p>
                     </div>
 
                     <div class="riga">
-                        <p>e-mail <input name="mail" type="text" /></p> 
-                        <p>Password <input name="password" type="password" /></p>
+                        <p>e-mail <input name="mail" type="text" required="required" value="<?php if($err) echo $_POST["mail"];?>"/></p> 
+                        <p>Password <input name="password" type="password" required="required" /></p>
                     </div>
 
                     <div class="rigaBottoni">
-                        <input type="reset" value="Cancella" />
-                        <input type="submit" value="Registrati" />
+                        <div class="sezioneErrore">
+                            <?php echo $ris ?>
+                        </div>
+                        <div class="sezioneBottoni">
+                            <input type="reset" value="Cancella" />
+                            <input type="submit" value="Registrati" />
+                        </div>
                     </div>
                 </div>
             </form>
