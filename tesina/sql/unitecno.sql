61c452d65eb4dcc5c5be3a907e4da95eced04f87
Matteo <matteo@DESKTOP-A44G1DR.broadband>
2024-06-07 19:33:54 +0200

    Fino al catalogo prodotti

---
+++ tesina/sql/unitecno.sql
@@ -0,0 +1,32 @@
+drop database if exists unitecno;
+create database unitecno;
+use unitecno;
+
+create table UTENTI(
+    id          integer auto_increment not null,
+    nome        varchar(50) not null,
+    cognome     varchar(50) not null,
+    indirizzo   varchar(70) not null,
+    citta       varchar(50) not null,
+    cap         varchar(5) not null,
+    reputazione integer default 1,
+    data_registrazione date,
+    stato       char(1) default 'A',
+    username    varchar(50) not null,
+    mail        varchar(50) not null,
+    password    char(64) not null,
+    ruolo   char(1) default 'C',
+    saldo_standard float(10, 2),
+    primary key (id),
+    unique (mail),
+    unique (username)
+);
+
+-- Inserimento di un account admin e di un account gestore
+insert into UTENTI(nome, cognome, indirizzo, citta, cap, reputazione, username, mail, password, ruolo) values
+                ('Matteo', 'Ventali', 'Via A. Caldara 8', 'Latina', '04100', 200, 'matteo.ventali', 'ventali@unitecno.it', 
+                    SHA2('unitecno', 256), 'A');
+
+insert into UTENTI(nome, cognome, indirizzo, citta, cap, reputazione, username, mail, password, ruolo) values
+                ('Stefano', 'Rosso', 'Via Santa Fecitola 2', 'Latina', '04100', 200, 'stefano.rosso', 'rosso@unitecno.it', 
+                    SHA2('unitecno', 256), 'G');
