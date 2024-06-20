db01dd1dd5fe21046fdd470e906e556c988793d6
Matteo <matteo@DESKTOP-A44G1DR.broadband>
2024-06-20 15:38:39 +0200

    Inizio sviluppo

---
+++ tesina/php/homepage.php
@@ -0,0 +1,27 @@
+<?php
+    echo '<?xml version = "1.0" encoding="UTF-8"?>';
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="../css/stileLayout.css" type="text/css" />
+        <link rel="stylesheet" href="../css/stileSlider.css" type="text/css" />
+        <link rel="icon" type="image/x-icon" href="../img/logo.png">
+        <title>UNI-TECNO</title>
+    </head>
+
+    <body>
+        <?php 
+            // Import della navbar
+            $nav = file_get_contents("../html/strutturaNavbarVisitatori.html");
+            echo $nav ."\n";
+
+            // Import dello slider del frammento di accesso al catalogo
+            $slider = file_get_contents("../html/frammentoAccessoCatalogo.html");
+            echo $slider . "\n";
+        ?>
+    </body>
+</html>
+    
