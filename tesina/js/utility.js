a179362c12e8c2ea2f2a0de139f747a1f4dc514e
Matteo <matteo@DESKTOP-A44G1DR.broadband>
2024-06-21 10:53:54 +0200

    Continuo implementazione

--- tesina/js/utility.js
+++ tesina/js/utility.js
@@ -1,9 +1,33 @@
 function scegliSfondoCasuale()
 {
-    nome_file = ["smartphone.jpg", "console.jpg", "elettrodomestici.jpg", "laptop.jpg", "televisore.jpg"];
-    scelta_casuale = Math.floor(Math.random() * 4);
+    nome_file = ["smartphone.jpg", "console.jpg", "elettrodomestici.jpg", "laptop.png", "televisore.jpg"];
+    scelta_casuale = Math.floor(Math.random() * 5);
 
     // Set del background tra quelli disponibili
     cat = document.getElementById("sezioneCatalogo");
     cat.style.backgroundImage="url('../img/background/" + nome_file[scelta_casuale] + "')";
+}
+
+function gestisciSidebar(param)
+{
+    // Prendo il riferimento alla sidebar
+    sidebar = document.getElementById("sidebar");
+
+    // Prendo il riferimento ai bottoni
+    btn1 = document.getElementById("btnSidebar1");
+    btn2 = document.getElementById("btnSidebar2");
+
+    // Verifico se la sidebar e' mostrata o meno tramite il testo 
+    if ( param == 0 ) // Mostro la sidebar
+    {
+        sidebar.style.left = "0%";
+        btn1.style.display = "none";
+        btn2.style.display = "block";
+    }
+    else if( param == 1 ) // Nascondo la sidebar
+    {
+        sidebar.style.left = "-25%";
+        btn1.style.display = "block";
+        btn2.style.display = "none";
+    }
 }
--- tesina/php/area_riservata.php
+++ tesina/php/area_riservata.php
@@ -7,7 +7,8 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="../css/stileLayout.css" type="text/css" />
-        <link rel="stylesheet" href="../css/stileSlider.css" type="text/css" />
+        <link rel="stylesheet" href="../css/stileCatalogo.css" type="text/css" />
+        <link rel="stylesheet" href="../css/stileSidebar.css" type="text/css" />
         <link rel="icon" type="image/x-icon" href="../img/logo.png" />
         <script type="text/javascript" src="../js/utility.js"></script>
         <title>UNI-TECNO</title>
@@ -22,6 +23,11 @@
             // Import del frammento di accesso al catalogo
             $cat = file_get_contents("../html/frammentoAccessoCatalogo.html");
             echo $cat . "\n";
+
+            // Import della sidebar e mostro le opzioni per l'utente loggato
+            $sidebar = file_get_contents("../html/strutturaSidebar.html");
+            //$sidebar = str_replace("%OPERAZIONI_UTENTE%", "", $sidebar);
+            echo $sidebar . "\n";
         ?>
     </body>
 </html>
